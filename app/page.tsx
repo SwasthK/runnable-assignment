9d8e935d100baf585edd5462fd9a745028be32ad
Swasthik K <swasth319@gmail.com>
2026-04-23 20:47:40 +0530

    feat: add input, preview and editor panel

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,35 @@
-import { Button } from "@/components/ui/button"
+"use client"
+
+import { useState, useEffect } from "react"
+import CodeInput from "@/components/code-input/code-input"
+import CodeEditor from "@/components/code-editor/code-editor"
+import Preview from "@/components/preview/preview"
 
 export default function Page() {
+  const [codeInputOpen, setCodeInputOpen] = useState(false)
+  const [codeEditorOpen, setCodeEditorOpen] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768 && codeInputOpen && codeEditorOpen) {
+        setCodeEditorOpen(false)
+      }
+    }
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [codeInputOpen, codeEditorOpen])
+
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex border h-full w-full">
+        <CodeInput open={codeInputOpen} setOpen={setCodeInputOpen} />
+        <Preview
+          codeInputOpen={codeInputOpen}
+          setCodeInputOpen={setCodeInputOpen}
+          codeEditorOpen={codeEditorOpen}
+          setCodeEditorOpen={setCodeEditorOpen}
+        />
+        <CodeEditor open={codeEditorOpen} setOpen={setCodeEditorOpen} />
       </div>
     </div>
   )
